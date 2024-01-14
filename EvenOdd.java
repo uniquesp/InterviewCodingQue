dd9ade056eb7c4cf27303906793ffe6e248e8233
Sakshi Pharande <71183249+uniquesp@users.noreply.github.com>
2024-01-14 10:44:39 +0530

    new commit

---
+++ EvenOdd.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+
+
+public class EvenOdd{
+
+    public static void evenodd(int n){
+        if(n%2==0){
+           System.out.println("Even");
+        }
+           System.out.println("Odd");
+    }
+
+    public static void main(String args[]){
+    Scanner s = new Scanner(System.in);
+    int n = s.nextInt();
+    evenodd(n);
+    }
+}
