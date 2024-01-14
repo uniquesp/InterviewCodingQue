c4819c315a68e8b3864f72d563a83fa1f47bd947
uniquesp <sakshisp1342002@gmail.com>
2024-01-14 10:53:10 +0530

    new commit

--- EvenOdd.java
+++ EvenOdd.java
@@ -1,14 +1,13 @@
 import java.util.*;
 
-
-
 public class EvenOdd{
 
     public static void evenodd(int n){
         if(n%2==0){
            System.out.println("Even");
+        }else{
+            System.out.println("Odd");
         }
-           System.out.println("Odd");
     }
 
     public static void main(String args[]){
