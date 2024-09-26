702c25097970b7ea80ff0db384cba11ee83613e7
uniquesp <sakshisp1342002@gmail.com>
2024-09-26 22:19:48 +0530

    26-09-24

---
+++ CodingQuetions/src/BasicQue/PalindromeNo.java
@@ -0,0 +1,28 @@
+package BasicQue;
+
+class PalindromeNo {
+
+	public static boolean isPalindrome(int num) {
+		if (num < 0) {
+			return false;
+		}
+		int original = num;
+		int reversed = 0;
+
+		while (num != 0) {
+			int digit = num % 10;
+			reversed = reversed * 10 + digit;
+			num /= 10;
+		}
+		return original == reversed;
+	}
+
+	public static void main(String[] args) {
+		int num = 121; 
+		if (isPalindrome(num)) {
+			System.out.println(num + " is a palindrome.");
+		} else {
+			System.out.println(num + " is not a palindrome.");
+		}
+	}
+}
